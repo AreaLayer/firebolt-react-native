28275f2f156a3d8717990c95876dbea6daf8772e
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-02-02 08:00:19 -0300

    Update MainActivity.kt

--- android/app/src/main/java/com/fireboltnew/MainActivity.kt
+++ android/app/src/main/java/com/fireboltnew/MainActivity.kt
@@ -6,6 +6,26 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.WritableMap
+import com.reactnativeldk.EventTypes
+import com.reactnativeldk.LdkEventEmitter
+import com.reactnativeldk.hexEncodedString
+import com.reactnativeldk.hexa
+import org.json.JSONObject
+import org.ldk.structs.Result_StrSecp256k1ErrorZ.Result_StrSecp256k1ErrorZ_OK
+import org.ldk.structs.UtilMethods
+import java.net.HttpURLConnection
+import java.net.URL
+import java.security.MessageDigest
+import java.security.SecureRandom
+import java.util.Date
+import java.util.Random
+import java.util.UUID
+import java.util.concurrent.locks.ReentrantLock
+import javax.crypto.Cipher
+import javax.crypto.spec.GCMParameterSpec
+import javax.crypto.spec.SecretKeySpec
 
 class MainActivity : ReactActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
