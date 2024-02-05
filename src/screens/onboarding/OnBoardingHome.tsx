4164cb25aaa7a4e254aaae885d25c4c7ad013afc
Fahad-Mahmood <fahad492@outlook.com>
2024-02-05 23:37:25 +0500

    refactor: native base removed and gluestack ui library added
    refactor: onboarding screen replaced with new ui components

--- src/screens/onboarding/OnBoardingHome.tsx
+++ src/screens/onboarding/OnBoardingHome.tsx
@@ -1,5 +1,14 @@
 import * as React from 'react';
-import {Box, Button, Image, Center, Heading} from 'native-base';
+import {
+  Box,
+  Button,
+  ButtonText,
+  Image,
+  Center,
+  Heading,
+} from '@gluestack-ui/themed';
+import {SCREEN_NAMES} from '../../navigation/screenNames';
+import {NavigationProp} from '@react-navigation/native';
 
 const LogoImage = require('../../assets/images/logo.png');
 
@@ -8,40 +17,48 @@ const HEADING_TEXT_2 = 'The ultimate privacy tool for bitcoiners';
 const CREATE_WALLET_BUTTON_TEXT = 'Create a new wallet';
 const RESTORE_WALLET_BUTTON_TEXT = 'Restore existing wallet';
 
-function OnBoardingHome() {
+interface Props {
+  navigation: NavigationProp<any, any>;
+}
+
+function OnBoardingHome({navigation}: Props) {
+  const onCreateWalletButtonPress = () => {
+    navigation.navigate(SCREEN_NAMES.CreateWallet);
+  };
   return (
-    <Box pb="30%" bgColor={'primary.900'} flex={1} justifyContent={'center'}>
+    <Box bg="$primary400" pb="30%" flex={1} justifyContent={'center'}>
       <Center>
-        <Image source={LogoImage} size="xl" />
-        <Heading mt={4} w="90%" size="2xl" color="white" textAlign={'center'}>
+        <Image alt="Firebolt Logo" source={LogoImage} size="xl" />
+        <Heading mt={'$4'} size="2xl" color="white" textAlign={'center'}>
           {HEADING_TEXT_1}
         </Heading>
-        <Heading
-          fontWeight={'400'}
-          mt={4}
-          w="90%"
-          size="md"
-          color="white"
-          textAlign={'center'}>
+        <Heading mt={'$4'} size="md" color="white" textAlign={'center'}>
           {HEADING_TEXT_2}
         </Heading>
         <Button
-          _text={{color: 'black', fontWeight: '700'}}
-          mt={10}
           size="lg"
-          w="90%"
-          colorScheme="secondary">
-          {CREATE_WALLET_BUTTON_TEXT}
+          mt={'$9'}
+          w={'$5/6'}
+          variant="solid"
+          action="secondary"
+          isDisabled={false}
+          isFocusVisible={false}
+          onPress={onCreateWalletButtonPress}>
+          <ButtonText color="black" fontWeight="$bold">
+            {CREATE_WALLET_BUTTON_TEXT}
+          </ButtonText>
         </Button>
         <Button
-          _text={{color: 'white', fontWeight: '700'}}
-          fontWeight={800}
-          mt={8}
-          variant="outline"
           size="lg"
-          w="90%"
-          colorScheme="secondary">
-          {RESTORE_WALLET_BUTTON_TEXT}
+          mt={'$7'}
+          w={'$5/6'}
+          variant="outline"
+          action="secondary"
+          isDisabled={false}
+          isFocusVisible={false}>
+          <ButtonText fontWeight="$bold">
+            {RESTORE_WALLET_BUTTON_TEXT}
+          </ButtonText>
         </Button>
       </Center>
     </Box>
