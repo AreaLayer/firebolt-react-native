fb104c445a86fd102f709522044903765b661b35
223880 <rafael.segala45@gmail.com>
2024-04-24 10:40:30 -0300

    Add P2P CJ Market

---
+++ src/navigation/CJMarket.tsx
@@ -0,0 +1,108 @@
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import OnBoardingHome from '../screens/Onboarding/OnBoardingHome';
+import CreateWallet from '../screens/Onboarding/CreateWallet';
+import { SCREEN_NAMES } from './screenNames';
+import ConfirmSeed from '../screens/Onboarding/ConfirmSeed';
+import PinSetup from '../screens/Onboarding/PinSetup';
+import ConfirmPin from '../screens/Onboarding/ConfirmPin';
+import Dashboard from '../screens/Home/Dashboard';
+import { useConnectionContext } from '../providers/ConnectionProvider';
+import { Splash } from '../components/Splash';
+import VerifyPin from '../screens/Home/VerifyPin';
+import CoinjoinMarket from '../screens/CoinjoinMarket/CoinjoinMarket';
+import OfferDetails from '../screens/CoinjoinMarket/OfferDetails';
+
+export type RootStackParamList = {
+  OnboardingHome: undefined;
+  CreateWallet: undefined;
+  ConfirmSeed: { words: string[] };
+  PinSetup: { words: string[] };
+  ConfirmPin: { words: string[]; walletPin: number[] };
+  Dashboard: undefined;
+  VerifyPin: undefined;
+  CoinjoinMarket: undefined;
+  OfferDetails: { offerId: string };
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function OnBoardingNavigation() {
+  const { loading, isWalletConnected } = useConnectionContext();
+  if (loading) {
+    return <Splash />;
+  }
+  return (
+    <Stack.Navigator>
+      {isWalletConnected ? (
+        <Stack.Group>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name={SCREEN_NAMES.VerifyPin}
+            component={VerifyPin}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name={SCREEN_NAMES.Dashboard}
+            component={Dashboard}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name={SCREEN_NAMES.CoinjoinMarket}
+            component={CoinjoinMarket}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name={SCREEN_NAMES.OfferDetails}
+            component={OfferDetails}
+          />
+        </Stack.Group>
+      ) : (
+        <Stack.Group>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name={SCREEN_NAMES.OnboardingHome}
+            component={OnBoardingHome}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name={SCREEN_NAMES.CreateWallet}
+            component={CreateWallet}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name={SCREEN_NAMES.ConfirmSeed}
+            component={ConfirmSeed}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name={SCREEN_NAMES.PinSetup}
+            component={PinSetup}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name={SCREEN_NAMES.ConfirmPin}
+            component={ConfirmPin}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name={SCREEN_NAMES.Dashboard}
+            component={Dashboard}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name={SCREEN_NAMES.CoinjoinMarket}
+            component={CoinjoinMarket}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name={SCREEN_NAMES.OfferDetails}
+            component={OfferDetails}
+          />
+        </Stack.Group>
+      )}
+    </Stack.Navigator>
+  );
+}
+
+export default OnBoardingNavigation;
