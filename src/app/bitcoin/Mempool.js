ea6da14d41a6ed71e1b327b5409fb61eac96fca1
223880 <rafael.segala45@gmail.com>
2024-04-30 14:11:33 -0300

    Add mempool with RBF

---
+++ src/app/bitcoin/Mempool.js
@@ -0,0 +1,15 @@
+import mempoolJS from '@mempool/mempool.js';
+
+// default mempool.space endpoints
+const { bitcoin } = mempoolJS();
+
+// (optional) your custom endpoints
+const { bitcoin } = mempoolJS({
+  hostname: 'mempool.space', 
+
+// broadcast transactions                     
+  const { tx } = tx({ 
+    
+
+const { rbf } = rbf();
+  rfb:  'fee';
