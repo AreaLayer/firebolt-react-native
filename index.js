ec90835a2c9db44b0e1fe8f086f5c8efa3c69ad3
223880 <rafael.segala45@gmail.com>
2024-05-13 13:19:19 -0300

    Fix

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ import './shim';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Coinjoin} from 'react-native-crypto'
 
 AppRegistry.registerComponent(appName, () => App);
