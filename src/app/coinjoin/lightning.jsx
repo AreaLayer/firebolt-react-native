14606436698e36f241baf27937a2bcffb998a3e1
223880 <rafael.segala45@gmail.com>
2024-05-29 14:29:18 -0300

    Fix

--- src/app/coinjoin/lightning.jsx
+++ src/app/coinjoin/lightning.jsx
@@ -1,7 +1,11 @@
 import {Payment, TXID, UTXO} from 'react-native-breez';
+import coinjoinxt = require('./coinjoinxt');
+
 
 const Payment = {
   Payment,
   TXID,
   UTXO
 };
+
+export default Payment;
