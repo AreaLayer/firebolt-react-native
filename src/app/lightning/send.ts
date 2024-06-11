f07b92b522657bfaf36f1941d0c0799d7f1af134
223880 <rafael.segala45@gmail.com>
2024-06-11 13:22:01 -0300

    Breez WIP

--- src/app/lightning/send.ts
+++ src/app/lightning/send.ts
@@ -1,1 +1,23 @@
-import {Balance, Payment} from 'react-native-breez-sdk';
+import {Balance, Payment, BackupStatus, BitcoinAddressData, CheckMessageRequest} from 'react-native-breez-sdk';
+
+export default class Breez {
+  static async getBalance(): Promise<Balance> {
+    return await BreezNative.getBalance();
+  }
+
+export async getPayment(): Promise<Payment> {
+    return await BreezNative.getPayment();
+  }
+
+  static async getBackupStatus(): Promise<BackupStatus> {
+    return await BreezNative.getBackupStatus();
+  }
+export async getBitcoinAddressData(): Promise<BitcoinAddressData> {
+    return await BreezNative.getBitcoinAddressData();
+  }
+export async checkMessage(request: CheckMessageRequest): Promise<boolean> {
+    return await BreezNative.checkMessage(request);
+  }
+  static async getVersion(): Promise<string> {
+    return await BreezNative.getVersion();
+  }
