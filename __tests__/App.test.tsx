335cb6af2842c908cdd156740f818d2d2795499d
223880 <rafael.segala45@gmail.com>
2024-06-17 11:03:10 -0300

    Add CJ tests

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -12,6 +12,10 @@ import {it} from '@jest/globals';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+// Note: import Coinjoin tests
+import coinjoin from '../src/app/coinjoin';
+
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
