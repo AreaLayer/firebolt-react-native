7b7d4b6f6afa32da39768a0bd00cf9e9539f0a5a
223880 <rafael.segala45@gmail.com>
2024-06-17 08:20:49 -0300

    Fix index and LSP

--- src/app/lightning/index.ts
+++ src/app/lightning/index.ts
@@ -1,1 +1,19 @@
 import {BOLT11, LSP, Network} from 'react-native-bree-sdk';
+
+export const generateInvoice = async (amount: number, memo: string, network: Network) => {
+  const invoice = await LSP.generateInvoice(amount, memo, network);
+  return BOLT11.encode(invoice);
+}
+export const decodeInvoice = async (invoice: string, network: Network) => {
+  const decodedInvoice = await BOLT11.decode(invoice);
+  return LSP.decodeInvoice(decodedInvoice, network);
+}
+
+export const encodeInvoice = async (invoice: LSP.Invoice, network: Network) => {
+  const encodedInvoice = await LSP.encodeInvoice(invoice, network);
+  return BOLT11.encode(encodedInvoice);
+}
+export const getBalance = async (network: Network) => {
+  const balance = await LSP.getBalance(network);
+  return balance;
+}
--- src/app/lightning/lsp.ts
+++ src/app/lightning/lsp.ts
@@ -1,1 +1,36 @@
 import as { LSP } from 'react-native-breez';
+
+export const generateInvoice = async (amount: number, memo: string, network: LSP.Network) => {
+  const invoice = await LSP.generateInvoice(amount, memo, network);
+  return invoice;
+}
+
+export const decodeInvoice = async (invoice: string, network: LSP.Network) => {
+  const decodedInvoice = await LSP.decodeInvoice(invoice, network);
+  return decodedInvoice;
+}
+
+export const encodeInvoice = async (invoice: LSP.Invoice, network: LSP.Network) => {
+  const encodedInvoice = await LSP.encodeInvoice(invoice, network);
+  return encodedInvoice;
+}
+
+export const getBalance = async (network: LSP.Network) => {
+  const balance = await LSP.getBalance(network);
+  return balance;
+}
+
+export const getAddress = async (network: LSP.Network) => {
+  const address = await LSP.getAddress(network);
+  return address;
+}
+
+export const getTransactions = async (network: LSP.Network) => {
+  const transactions = await LSP.getTransactions(network);
+  return transactions;
+}
+
+export const getTransaction = async (txid: string, network: LSP.Network) => {
+  const transaction = await LSP.getTransaction(txid, network);
+  return transaction;
+}
