fc668118479af5fbdd4ee49a621212be64b1244a
223880 <rafael.segala45@gmail.com>
2024-06-22 08:31:56 -0300

    Initial Nostr profile

---
+++ src/utils/nostr/encryption.ts
@@ -0,0 +1,15 @@
+import {NDKUserProfile} from "./NDKUserProfile";
+import {Name, NIP05, ZapService} from "./NDKUserProfile";
+
+export const interface = {
+    name: String,
+    nip05: String,
+    zapService: String,
+
+export const profilefromevent = {
+    name: String,
+    nip05: String,
+    zapService: String,
+}
+
+}
