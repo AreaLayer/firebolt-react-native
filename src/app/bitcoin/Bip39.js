e8f7d9f02a6cc6fab64f24f719da336665139d8d
223880 <rafael.segala45@gmail.com>
2024-06-22 07:32:32 -0300

    Add 256 bits and 128 bits for 24 & 12 words

--- src/app/bitcoin/Bip39.js
+++ src/app/bitcoin/Bip39.js
@@ -1,31 +1,18 @@
-const {mnemonic, seed, mnemonicToSeedSync} = require('bip39');
+const bip39 = require('bip39');
 
 // Generate a random mnemonic (uses crypto.randomBytes under the hood), defaults to 128-bits of entropy
-const mnemonic = bip39.generateMnemonic();
-console.log("Generated Mnemonic: ", mnemonic);
+const mnemonic128 = bip39.generateMnemonic();
+console.log("Generated Mnemonic (128-bits): ", mnemonic128);
 
 // Convert mnemonic to seed
-const seed = bip39.mnemonicToSeedSync(mnemonic).toString('hex');
-console.log("Corresponding Seed: ", seed);
-
-const bip39 = require('bip39');
+const seed128 = bip39.mnemonicToSeedSync(mnemonic128).toString('hex');
+console.log("Corresponding Seed (128-bits): ", seed128);
 
 // Generate a random mnemonic with 256 bits of entropy
-const mnemonic = bip39.generateMnemonic(256);
-console.log("Generated Mnemonic: ", mnemonic);
+const mnemonic256 = bip39.generateMnemonic(256);
+console.log("Generated Mnemonic (256-bits): ", mnemonic256);
 
 // Convert mnemonic to seed
-const seed = bip39.mnemonicToSeedSync(mnemonic).toString('hex');
-console.log("Corresponding Seed: ", seed);
+const seed256 = bip39.mnemonicToSeedSync(mnemonic256).toString('hex');
+console.log("Corresponding Seed (256-bits): ", seed256);
 
-const mnemonicToSeedSync = require('bip39').mnemonicToSeedSync;
-
-// Generate a random mnemonic with 256 bits of entropy
-const mnemonic = bip39.generateMnemonic(256);
-console.log("Generated Mnemonic: ", mnemonic);
-
-// Convert mnemonic to seed
-const seed = mnemonicToSeedSync(mnemonic).toString('hex');
-console.log("Corresponding Seed: ", seed);
-
-const bip39 = require('bip39');
