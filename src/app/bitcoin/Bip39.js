ba8c00a315c8aa2a17da58d897cee457cb43a39c
223880 <rafael.segala45@gmail.com>
2024-06-22 07:21:48 -0300

    Fix BIP39

--- src/app/bitcoin/Bip39.js
+++ src/app/bitcoin/Bip39.js
@@ -1,4 +1,4 @@
-const bip39 = require('bip39');
+const {mnemonic, seed, mnemonicToSeedSync} = require('bip39');
 
 // Generate a random mnemonic (uses crypto.randomBytes under the hood), defaults to 128-bits of entropy
 const mnemonic = bip39.generateMnemonic();
@@ -17,3 +17,15 @@ console.log("Generated Mnemonic: ", mnemonic);
 // Convert mnemonic to seed
 const seed = bip39.mnemonicToSeedSync(mnemonic).toString('hex');
 console.log("Corresponding Seed: ", seed);
+
+const mnemonicToSeedSync = require('bip39').mnemonicToSeedSync;
+
+// Generate a random mnemonic with 256 bits of entropy
+const mnemonic = bip39.generateMnemonic(256);
+console.log("Generated Mnemonic: ", mnemonic);
+
+// Convert mnemonic to seed
+const seed = mnemonicToSeedSync(mnemonic).toString('hex');
+console.log("Corresponding Seed: ", seed);
+
+const bip39 = require('bip39');
