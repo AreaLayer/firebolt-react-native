673c77b06c439c584ca9c30dd5505a14f6a51bbc
223880 <rafael.segala45@gmail.com>
2024-06-24 08:15:22 -0300

    Fix lightning payments

--- src/app/lightning/receive.ts
+++ src/app/lightning/receive.ts
@@ -1,5 +1,23 @@
-import {Payment} from 'react-native-breez-sdk';
+import {Payment} from '@react-native-breez-sdk/NativeModules'
+import {Receive} from '@react-native-breez-sdk/NativeModules'
 
 export const payment = {
+    getInvoice: async (amount: number, memo: string) => {
+        return await Payment.getInvoice(amount, memo)
+    },
+    payInvoice: async (invoice: string) => {
+        return await Payment.payInvoice(invoice)
+    }
+    receiveInvoice: async (invoice: string) => {
+        return await Receive.receiveInvoice(invoice)
+    },
+
+    // getAddress: async () => {
+    //     return await Receive.getAddress()
+    // },
+    // getBalance: async () => {
+    
+
+
     
 }
