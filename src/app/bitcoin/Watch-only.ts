2a54e9d4dd6b681c0f4c995e80f29a1cb6069ffa
223880 <rafael.segala45@gmail.com>
2024-06-29 07:17:31 -0300

    Fix

--- src/app/bitcoin/Watch-only.ts
+++ src/app/bitcoin/Watch-only.ts
@@ -1,1 +1,9 @@
+import { Bitcoin } from './Bitcoin';
+import { WatchOnly } from './WatchOnly';
 
+export { Bitcoin, WatchOnly };
+
+export default { Bitcoin, WatchOnly };
+
+export { default as Bitcoin } from './Bitcoin';
+export { default as WatchOnly } from './WatchOnly';
