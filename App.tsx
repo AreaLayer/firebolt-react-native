ce8f33fd3b6e605c5f9cf342de81b52df2c3f745
223880 <rafael.segala45@gmail.com>
2024-07-02 14:53:22 -0300

    Fix

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import {GluestackUIProvider} from '@gluestack-ui/themed';
+// No changes needed, 'react' is a core module and does not need to be imported explicitlyimport {GluestackUIProvider} from '@gluestack/ui';
 import {config} from './src/theme/config';
 
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import OnBoardingNavigation from './src/navigation/OnBoarding';
 import {ConnectionProvider} from './src/providers/ConnectionProvider';
 import OnBoardingHome from './src/screens/onboarding/CoinjoinMarket';
