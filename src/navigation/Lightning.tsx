3abfca2e9d625c9be78cbe79c4f3d09402795786
223880 <rafael.segala45@gmail.com>
2024-07-24 12:12:27 -0300

    Add LN navigation

--- src/navigation/Lightning.tsx
+++ src/navigation/Lightning.tsx
@@ -1,1 +1,16 @@
+import React, { useEffect } from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+const LightningPaymment = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="LightningPaymment" component={LightningPaymment} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default LightningPaymment;
 
