f6a4e9d46ab5d4aa8179320f18f75408c4ab9caf
223880 <rafael.segala45@gmail.com>
2024-08-03 06:57:36 -0300

    Add Node settings

---
+++ src/navigation/NodeSettings.tsx
@@ -0,0 +1,9 @@
+import react from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+const NodeSettings = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Node Settings</Text>
+    </View>
+  );
+};
