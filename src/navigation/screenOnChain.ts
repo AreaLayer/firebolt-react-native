d058f682322a7ca4cdc17aa3d04e9a441c56fe87
223880 <rafael.segala45@gmail.com>
2024-08-06 06:26:57 -0300

    Fix

--- src/navigation/screenOnChain.ts
+++ src/navigation/screenOnChain.ts
@@ -1,9 +1,14 @@
 import { generateInvoice } from "../app/lightning";
 import { SCREEN_ONX_ADDRESS } from "../app/constants";
 
-OnboardingHome: 'OnboardingHome';
-Send: 'Send';
-Receive: 'Receive';
-generateInvoice: 'generateInvoice';
-generateAddress: 'generateAddress';
+interface ScreenOnChain {
+  [SCREEN_ONX_ADDRESS]: {
+    invoice: string;
+  };
 }
+interface {
+  OnboardingHome: 'OnboardingHome';
+  Send: 'Send';
+  Receive: 'Receive';
+  generateInvoice: 'generateInvoice';
+  generateAddress: 'generateAddress';
