f356e31f8e11ccb3ca2b708d17e3c644f3dbf799
223880 <rafael.segala45@gmail.com>
2024-08-07 11:55:57 -0300

    Fix and add LSP support

---
+++ src/navigation/LSP.tsx
@@ -0,0 +1,11 @@
+import { LSP } from 'firebolt-react-native';
+import { View, Text } from 'react-native';
+
+export const LSPExample = () => {
+  return (
+    <View>
+      <Text>LSP Example</Text>
+      <LSP />
+    </View>
+  );
+};
--- src/navigation/NodeSettings.tsx
+++ src/navigation/NodeSettings.tsx
@@ -1,9 +1,9 @@
-import react from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 const NodeSettings = () => {
   return (
     <View style={styles.container}>
       <Text>Node Settings</Text>
+      <Text>Add Electrum Node</Text>
     </View>
   );
 };
