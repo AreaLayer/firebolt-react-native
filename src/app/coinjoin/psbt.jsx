777854d8fc05b3df43972e8d410e9ff8726ba6a0
223880 <rafael.segala45@gmail.com>
2024-08-09 15:33:16 -0300

    Fix PSBT

--- src/app/coinjoin/psbt.jsx
+++ src/app/coinjoin/psbt.jsx
@@ -1,4 +1,4 @@
-const { bip371, psbtutils} = require('bitcoinjs-lib');
+const { Bip371, Psbtutils, Psbt} = require('bitcoinjs-lib');
 
 const TapscriptFinalizer {
   constructor(psbt, tapscript) {
@@ -6,5 +6,14 @@ const TapscriptFinalizer {
         this.tapscript = tapscript;
     }
 }
+
+const TapscriptFinalizer.prototype.finalize = function() {
+    const psbt = this.psbt;
+    const tapscript = this.tapscript;
+    const tapscript_finalizer = new Bip371.TapscriptFinalizer(psbt, tapscript);
+    const final_psbt = tapscript_finalizer.finalize();
+    return final_psbt;
+
+const Psbt = require('bitcoinjs-lib').Psbt;
 
 module.exports = TapscriptFinalizer;
