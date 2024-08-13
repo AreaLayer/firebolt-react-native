3c2c51fb447dd60c3f86e2095dc4ff3176248998
223880 <rafael.segala45@gmail.com>
2024-08-13 10:14:22 -0300

    Fix

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import './shim';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import { Coinjoin as CoinjoinApp } from './src/coinjoin';
+import {coinjoin as Coinjoin } from './src/coinjoin';
+import {firebolt as Firebolt } from './src/firebolt';
 
 AppRegistry.registerComponent(appName, () => App);
