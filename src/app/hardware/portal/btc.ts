f171cbc8f73b47d66042de148d002d397439afac
223880 <rafael.segala45@gmail.com>
2024-08-27 13:45:27 -0300

    Add PSBT

--- src/app/hardware/portal/btc.ts
+++ src/app/hardware/portal/btc.ts
@@ -1,1 +1,11 @@
 import { PSBT } from 'bitcoinjs-lib';
+
+export class Btc {
+  static async getBtcAddress(psbt: PSBT): Promise<string> {
+    return 'TODO';
+  }
+}
+
+
+export const psbt = new PSBT();
+
