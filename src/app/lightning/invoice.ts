4b945e5df31bf43bcc4bd3498d9aaf5aa3d105f5
223880 <rafael.segala45@gmail.com>
2024-08-27 12:37:20 -0300

    Add invoice

---
+++ src/app/lightning/invoice.ts
@@ -0,0 +1,30 @@
+import { LightningInvoice } from '@/types/lightning'
+
+export class Invoice {
+    id: string
+    amount: number
+    description: string
+    date: string
+    status: string
+    paid: boolean
+}
+export interface LightningInvoiceState {
+    invoices: LightningInvoice[]
+}
+
+export const state = (): LightningInvoiceState => ({
+    invoices: []
+})
+
+export const mutations = {
+    setInvoices(state: LightningInvoiceState, invoices: LightningInvoice[]) {
+        state.invoices = invoices
+    }
+}
+
+export const actions = {
+    async fetchInvoices({ commit }: any, { pubkey }: { pubkey: string }) {
+        const invoices = await this.$axios.$get(`/api/lightning/invoices/${pubkey}`)
+        commit('setInvoices', invoices)
+    }
+}
