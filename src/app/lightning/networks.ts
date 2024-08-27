813ab325d526d53f188336e36c329827b661f0fa
223880 <rafael.segala45@gmail.com>
2024-08-27 09:02:21 -0300

    Fix network

--- src/app/lightning/networks.ts
+++ src/app/lightning/networks.ts
@@ -1,7 +1,10 @@
 import {Mainnet, Testnet, Signet} from ('bitcoinjs-lib');
 
-const = {
-var network: Mainnet;
-var network: Testnet;
-var network:  Signet;
+
+export class Networks {
+    constructor(network: Mainnet, network: Testnet, network: Signet) {
+        this.network = network;
+        this.network = network;
+        this.network = network;
+    }
 }
