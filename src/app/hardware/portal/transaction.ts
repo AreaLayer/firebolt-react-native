2e28b285b72f3b27de38bacae92d2f657e7b8f68
223880 <rafael.segala45@gmail.com>
2024-09-03 08:47:43 -0300

    Transaction WIP

--- src/app/hardware/portal/transaction.ts
+++ src/app/hardware/portal/transaction.ts
@@ -1,1 +1,15 @@
 import { ITransaction } from './ITransaction';
+
+export class Transaction implements ITransaction {
+    id: number;
+    name: string;
+    description: string;
+    amount: number;
+    date: Date;
+    type: string;
+    category: string;
+    account: string;
+    status: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
