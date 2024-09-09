910562ae1bd44ded36ad6ab182189a34ab5283bb
223880 <rafael.segala45@gmail.com>
2024-09-09 13:08:50 -0300

    Fix

--- src/lib/sp.ts
+++ src/lib/sp.ts
@@ -1,2 +1,11 @@
 import { NativeModules } from 'react-native';
 import { SilentPaymentGroup } from '../app/SP/send';
+
+export const { SP } = NativeModules;
+
+export default SP;
+
+
+export const SilentPaymentGroups = {
+    [SilentPaymentGroup.SP_SILENT_PAYMENT_GROUP_1]: 'SP_SILENT_PAYMENT_GROUP_1',
+}
