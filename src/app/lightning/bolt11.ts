9c74a745879da74dcca703020e4b7bfc2e067315
223880 <rafael.segala45@gmail.com>
2024-09-13 07:50:52 -0300

    Fix BOLT11

--- src/app/lightning/bolt11.ts
+++ src/app/lightning/bolt11.ts
@@ -1,21 +1,16 @@
-import {sendPayment} from '../lightning';
-import { Bolt11 } from '../lightning'
+import { BOLT11, Invoice } from '@breeztech/react-native-breez-sdk';
 
 const BOLT11 = {
 
-Payment: String,
-Invoice: String,
-Amount: Number,
+  Payment: String,
+  Invoice: String,
+  Amount: Number,
+  }
     
-};
-    const bolt11 = 'bolt11 invoice'
-    const optionalAmountMsat = 3000000
-    const optionalLabel = '<label>'
-    const response = await sendPayment({
-      bolt11,
-      amountMsat: optionalAmountMsat,
-      label: optionalLabel
-    })
-    console.log(response)
-  
-  
+const optionalAmountMsat = 3000000
+const optionalLabel = '<label>'
+const response = await sendPayment({
+  bolt11,
+  amountMsat: optionalAmountMsat,
+  label: optionalLabel
+})
