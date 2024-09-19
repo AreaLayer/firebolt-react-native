6d3a3a6eebbcdd776a384ac61731a44ea018f8b2
223880 <rafael.segala45@gmail.com>
2024-09-19 13:33:39 -0300

    Fix liquid address data

--- src/app/liquidwallet/address.ts
+++ src/app/liquidwallet/address.ts
@@ -1,20 +1,9 @@
 import { LiquidNetwork, LiquidAddressData } from "@breeztech/react-native-breez-sdk-liquid";
 
-export const getAddressData = async (network: LiquidNetwork, address: string): Promise<LiquidAddressData> => {
-    return await LiquidNetwork.getAddressData(network, address);
+export const LiquidgetAddressData = async (network: LiquidNetwork, address: string): Promise<LiquidAddressData> => {
+    return await LiquidNetwork.getLiquidAddressData(network, address);
 }
-
-export const getAddressDataFromBIP44 = async (network: LiquidNetwork, bip44Path: string): Promise<LiquidAddressData> => {
-    return await LiquidNetwork.getAddressDataFromBIP44(network, bip44Path);
-}
-
-export const getAddressDataFromBIP44Path = async (network: LiquidNetwork, bip44Path: string): Promise<LiquidAddressData> => {
-    return await LiquidNetwork.getAddressDataFromBIP44Path(network, bip44Path);
-}
-
 
 export const LiquidNetworkUtils = {
-    getAddressData,
-    getAddressDataFromBIP44,
-    getAddressDataFromBIP44Path,
+    LiquidgetAddressData,
 }
