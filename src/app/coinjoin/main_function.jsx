17d03c966bb5e7ce9bea4f3ce302f98f22883f28
223880 <rafael.segala45@gmail.com>
2024-09-19 10:38:39 -0300

    Fix CJ

--- src/app/coinjoin/main_function.jsx
+++ src/app/coinjoin/main_function.jsx
@@ -1,7 +1,7 @@
-const {Transaction, Psbt, txDetails} = require('bitcoinjs-lib');
-const {TX}= require('@mempool/mempool.js');
+const { Transaction, Psbt, txDetails } = require('bitcoinjs-lib');
+const { TX } = require('@mempool/mempool.js');
 const { BitcoinConverter } = require('./bitcoin_converter.json');
-const { Coinjoin, CoinjoinTransaction, createMultisigTransaction, createP2PTransaction, } = require('./main_function.jsx');
+const { Coinjoin, CoinjoinTransaction, createMultisigTransaction, createP2PTransaction } = require('./main_function.jsx');
 
 // Connect to the Bitcoin signet network
 const provider = 'https://signet.mempool.space/api';
@@ -28,40 +28,35 @@ class Coinjoin {
 }
 
 class CoinjoinTransaction {
-    constructor(txid, vout, amount, address) {
-        this.txid = txid;
-        this.vout = vout;
-        this.amount = amount;
-        this.address = address;
-    }
+  constructor(txid, vout, amount, address) {
+    this.txid = txid;
+    this.vout = vout;
+    this.amount = amount;
+    this.address = address;
+  }
 }
-
 
 // Function to create a Coinjoin transaction
 async function createCoinjoinTransaction(inputs, outputs) {
   const tx = new TX();
   const txid = tx.createCoinjoinTransaction(inputs, outputs);
   return txid;
-  // const txid = await provider.send('createCoinjoinTransaction', {
-  //   inputs,
-  //   outputs,
-  // });
-};
+}
 
 // Multisig and PSBT transaction
 async function createMultisigTransaction(inputs, outputs) {
   const tx = new TX();
   const txid = tx.createMultisigTransaction(inputs, outputs);
   return txid;
-};
+}
 
 // Peer to Peer transaction
-
 async function createP2PTransaction(inputs, outputs) {
-    const tx = new TX();
-    const txid = tx.createP2PTransaction(inputs, outputs);
-    return txid;
+  const tx = new TX();
+  const txid = tx.createP2PTransaction(inputs, outputs);
+  return txid;
 }
+
 // Function to get the transaction details
 async function getTransactionDetails(txid) {
   const tx = new TX();
@@ -69,7 +64,7 @@ async function getTransactionDetails(txid) {
   return txDetails;
 }
 
-// Funtion to PSBT sign
+// Function to sign PSBT
 async function signPSBT(psbt) {
   const tx = new TX();
   const signedPSBT = tx.signPSBT(psbt);
@@ -83,18 +78,60 @@ async function broadcastTransaction(txid) {
   return broadcastedTx;
 }
 
-// Funtion to brodcast the transation with mempool tx
+// Function to broadcast the transaction with mempool TX
 async function broadcastTransactionWithMempoolTx(txid) {
   const tx = new TX();
   const broadcastedTx = tx.broadcastTransactionWithMempoolTx(txid);
   return broadcastedTx;
 }
 
-// Function to get the transaction details
-async function getTransactionDetails(txid) {
+// Function to validate Coinjoin inputs
+async function validateCoinjoinInputs(inputs) {
   const tx = new TX();
-  const txDetails = tx.getTransactionDetails(txid);
-  return txDetails;
+  const validInputs = [];
+  for (const input of inputs) {
+    const isUnspent = await tx.checkIfUnspent(input.txid, input.vout);
+    if (isUnspent) {
+      validInputs.push(input);
+    }
+  }
+  return validInputs;
+}
+
+// Function to calculate transaction fee
+async function calculateTransactionFee(inputs, outputs, feeRatePerByte = 10) {
+  const tx = new TX();
+  const estimatedSize = tx.estimateTransactionSize(inputs, outputs);
+  const fee = estimatedSize * feeRatePerByte;
+  return fee;
+}
+
+// Function to add an input to Coinjoin
+async function addInputToCoinjoin(coinjoinTransaction, newInput) {
+  coinjoinTransaction.inputs.push(newInput);
+  return coinjoinTransaction;
+}
+
+// Function to add an output to Coinjoin
+async function addOutputToCoinjoin(coinjoinTransaction, newOutput) {
+  coinjoinTransaction.outputs.push(newOutput);
+  return coinjoinTransaction;
+}
+
+// Function to finalize Coinjoin transaction
+async function finalizeCoinjoinTransaction(coinjoinTransaction) {
+  const tx = new TX();
+  const finalizedTx = tx.finalizeTransaction(coinjoinTransaction);
+  return finalizedTx;
+}
+
+// Function to split the change output
+function splitChangeOutput(changeOutput, participants) {
+  const amountPerParticipant = changeOutput.amount / participants.length;
+  return participants.map(participant => ({
+    address: participant.address,
+    amount: amountPerParticipant,
+  }));
 }
 
 // ZK proofs
