9d2c4c00dcf1d3266ab73774e5f117bfa3069dec
223880 <rafael.segala45@gmail.com>
2024-09-19 08:57:25 -0300

    Add backup UI

---
+++ src/components/Backup.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import BIP39 from 'bip39';
+
+const app:::backup = () => {
+  const [backup, setBackup] = useState('');
+  const [backupError, setBackupError] = useState('');
+  const [backupSuccess, setBackupSuccess] = useState('');
+  const [backupLoading, setBackupLoading] = useState(false);
+  const [backupCompleted, setBackupCompleted] = useState(false);
+}
