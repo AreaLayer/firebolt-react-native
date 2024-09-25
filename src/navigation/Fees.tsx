09737b932e1463d44588002aee31ef036a2ab940
223880 <rafael.segala45@gmail.com>
2024-09-25 08:39:39 -0300

    Add mempool blocks

--- src/navigation/Fees.tsx
+++ src/navigation/Fees.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from'react';
 import { NavigationContainer } from '@react-navigation/native';
-
+import { FeesMempoolBlocks } from '@mempool/mempool.js/lib/interfaces/bitcoin/fees';
 
 const App = () => {
     return (
@@ -8,6 +8,7 @@ const App = () => {
             <Stack.Navigator>
                 <Stack.Screen name="Fees" component={Fees} />
                 <Stack.Screen name="Miner Fees" component={MinerFees} />
+                <Stack.Screen name="Fees Mempool Blocks" component={FeesMempoolBlocks} />
                 <App.Screen name="Home" component={Home} />
             </Stack.Navigator>
         </NavigationContainer>
