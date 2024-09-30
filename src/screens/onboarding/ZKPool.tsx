d032029f735a416dae568e9a0d7392b6182eda94
223880 <rafael.segala45@gmail.com>
2024-09-30 05:59:23 -0300

    Fix

--- src/screens/onboarding/ZKPool.tsx
+++ src/screens/onboarding/ZKPool.tsx
@@ -9,6 +9,7 @@ import {
 } from '@gluestack-ui/themed';
 import {SCREEN_NAMES} from '../../navigation/screenNames';
 import {NavigationProp} from '@react-navigation/native';
+import { MainFunction } from '../../src/coinjoin/main_function';
 
 const LogoImage = require('../../assets/images/logo.png');
 
