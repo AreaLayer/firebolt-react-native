292a0406deb70e2e02dab890667917c780219172
223880 <rafael.segala45@gmail.com>
2024-10-01 12:49:15 -0300

    Fix

--- src/app/bitcoin/Payment.js
+++ src/app/bitcoin/Payment.js
@@ -1,2 +1,9 @@
 import { Payment } from './payment';
 
+const  payment = new Payment();
+
+const paymement = {
+    p2tr: payment.p2tr,
+    p2wpkh: payment.p2wpkh,
+    p2wsh: payment.p2wsh,
+}
--- src/app/bitcoin/Watch-only.ts
+++
@@ -1,9 +0,0 @@
-import { Bitcoin } from './Bitcoin';
-import { WatchOnly } from './WatchOnly';
-
-export { Bitcoin, WatchOnly };
-
-export default { Bitcoin, WatchOnly };
-
-export { default as Bitcoin } from './Bitcoin';
-export { default as WatchOnly } from './WatchOnly';
