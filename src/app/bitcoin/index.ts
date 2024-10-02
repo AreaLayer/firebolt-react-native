c07fbc48b961e90fcb002d51cdea41ce813e103c
223880 <rafael.segala45@gmail.com>
2024-10-02 06:05:13 -0300

    Fix

--- src/app/bitcoin/index.ts
+++ src/app/bitcoin/index.ts
@@ -1,6 +1,5 @@
 import { IUser } from './user.interface';
 import { User } from './user.entity';
-import { Bitcoin } from './Watch-only';
 
 const user: IUser = {
   id: 1,
@@ -36,6 +35,8 @@ describe('User.repository', () => {
   it('should be defined', () => {
     expect(UsersRepository).toBeDefined();
   });
+}
 
   it('should be able to find a user', async () => {
     const user = await UsersRepository.findOne(1);
+  }
