942eefbaf8c9193cc6f66a4d623e2ae3650e5f10
223880 <rafael.segala45@gmail.com>
2024-10-10 08:02:22 -0300

    Add sats balance

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -3,13 +3,15 @@ import React from 'react';
 interface BalanceProps {
   balance: number;
   fiatEquivalent: number;
+  satsEquivalent: number;
 }
 
-const Balance: React.FC<BalanceProps> = ({ balance, fiatEquivalent }) => (
+const Balance: React.FC<BalanceProps> = ({ balance, fiatEquivalent , satsEquivalent}) => (
   <div className="balance">
     <h2>Your Balance</h2>
     <p>{balance} sats</p>
     <p>{fiatEquivalent} €</p>
+    <p>{satsEquivalent} sats</p>
   </div>
 );
 
