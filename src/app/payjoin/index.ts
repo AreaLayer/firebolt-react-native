89182e4849b37496804b7cc2cd5e61a9cc0ece8e
223880 <rafael.segala45@gmail.com>
2024-10-16 06:24:18 -0300

    Fix Pj

--- src/app/payjoin/index.ts
+++ src/app/payjoin/index.ts
@@ -1,8 +1,12 @@
-import { PayJoin } from './payjoin';
+// Import PayJoin from your local file
+import PayJoin from './payjoin';
+
+// Import PayjoinURL from the 'payjoin-react-native' package
 import { PayjoinURL } from 'payjoin-react-native';
 
+// Export PayJoin and PayjoinURL
 export default PayJoin;
-
 export { PayjoinURL };
+
 
 
