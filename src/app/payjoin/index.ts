981a44f4035a0b170730bf99dd07252fe72d5fe0
223880 <rafael.segala45@gmail.com>
2024-10-16 06:38:10 -0300

    Fix

--- src/app/payjoin/index.ts
+++ src/app/payjoin/index.ts
@@ -1,11 +1,7 @@
-// Import PayJoin from your local file
-import PayJoin from './payjoin';
-
 // Import PayjoinURL from the 'payjoin-react-native' package
 import { PayjoinURL } from 'payjoin-react-native';
 
 // Export PayJoin and PayjoinURL
-export default PayJoin;
 export { PayjoinURL };
 
 
---
+++ src/types/payjoin-react-native.d.ts
@@ -0,0 +1,9 @@
+// src/types/payjoin-react-native.d.ts
+declare module 'payjoin-react-native' {
+    export class PayjoinURL {
+      constructor(url: string);
+      // Add any other methods or properties you know PayjoinURL uses
+      parse(): object;  // Example method
+    }
+  }
+  
