8611a7536cac0372efab9da777f64edf349d9c23
223880 <rafael.segala45@gmail.com>
2024-11-02 06:17:19 -0300

    Fix PJ

--- src/app/payjoin/index.ts
+++ src/app/payjoin/index.ts
@@ -1,6 +1,12 @@
 // Import PayjoinURL from the 'payjoin-react-native' package
 import { PayjoinURL } from 'payjoin-react-native';
 
+export const PayJoin = (url: string) => {
+  // Create a PayjoinURL instance
+  const payjoinURL = new PayjoinURL(url);
+}
+
+
 // Export PayJoin and PayjoinURL
 export { PayjoinURL };
 
