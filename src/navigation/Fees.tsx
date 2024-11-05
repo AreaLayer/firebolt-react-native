df69b72ac670a3002ce6a6fa8013dd062d9d3436
223880 <rafael.segala45@gmail.com>
2024-11-05 11:06:29 -0300

    Fix fees

--- src/navigation/Fees.tsx
+++ src/navigation/Fees.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack'; // Import Stack Navigator
 import Fees from './path/to/Fees'; // Adjust paths as necessary
-import MinerFees from './path/to/MinerFees'; // Adjust paths as necessary
+import MinerFees from '../components/MinerFees';
 import FeesMempoolBlocks from './path/to/FeesMempoolBlocks'; // Adjust paths as necessary
 import Home from './path/to/Home'; // Adjust paths as necessary
 
