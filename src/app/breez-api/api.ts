e44c763ffa597ae0b14fc8302a16370c73664e0b
223880 <rafael.segala45@gmail.com>
2024-11-22 08:51:00 -0300

    Fix

---
+++ src/app/breez-api/api.ts
@@ -0,0 +1,20 @@
+
+import {
+  GreenlightCredentials,
+  NodeConfig,
+  NodeConfigVariant
+} from '@breeztech/react-native-breez-sdk';
+
+const developerKey: number[] = []
+const developerCert: number[] = []
+const greenlightCredentials: GreenlightCredentials = {
+  developerKey,
+  developerCert
+}
+
+// const nodeConfig: NodeConfig = {
+//   type: NodeConfigVariant.GREENLIGHT,
+//   config: {
+//     partnerCredentials: greenlightCredentials
+//   }
+// }
