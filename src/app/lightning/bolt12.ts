88e0facbea0628ed37f3aa097754e4f918129328
22388o <22388o@users.noreply.github.com>
2024-11-28 09:06:43 -0300

    Fix BOLT12

--- src/app/lightning/bolt12.ts
+++ src/app/lightning/bolt12.ts
@@ -1,9 +1,11 @@
-{
- LnOffer,
- Bolt12,
-}
-from "@breeztech/react-native-breez-sdk-liquid";
+import { Bolt12 } from "@breeztech/react-native-breez-sdk-liquid";
 
 const LnOffer = {
   bolt12: Bolt12,
 };
+
+export type LnOffer = typeof LnOffer;
+
+export const bolt12 = LnOffer.bolt12;
+
+export default LnOffer;
