5d3681e7c5fed4ddd32396bd4746a6dbff467aef
22388o <22388o@users.noreply.github.com>
2025-01-10 13:25:43 -0300

    Fix

--- src/app/liquidwallet/address.ts
+++ src/app/liquidwallet/address.ts
@@ -1,9 +1,8 @@
 import { LiquidNetwork, LiquidAddressData } from "@breeztech/react-native-breez-sdk-liquid";
 
 export const LiquidgetAddressData = async (network: LiquidNetwork, address: string): Promise<LiquidAddressData> => {
-    return await LiquidNetwork.getAddressData(network, address);
+    return await LiquidAddressData.getAddressData(network, address);
 }
-
 export const LiquidNetworkUtils = {
     LiquidgetAddressData,
 }
