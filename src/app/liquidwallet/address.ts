9c12dca8a570fbe0a657bb7ab1bcf76ed48058bd
22388o <22388o@users.noreply.github.com>
2025-02-21 07:27:17 -0300

    Add L-BTC balance and contract

--- src/app/liquidwallet/address.ts
+++ src/app/liquidwallet/address.ts
@@ -1,4 +1,4 @@
-import { LiquidNetwork, LiquidAddressData, ListPaymentsRequest } from "@breeztech/react-native-breez-sdk-liquid";
+import { LiquidNetwork, LiquidAddressData, ListPaymentsRequest, AssetMetadata } from "@breeztech/react-native-breez-sdk-liquid";
 
 import { SwapLiquidInterface } from "../swap/liquid";
 
@@ -37,4 +37,15 @@ export const explorer = {
 export const BreezAPI = {
     mainnet: "https://api.breez.technology/v2",
     testnet: "https://api.breez.technology/v1",
+}
+
+export const assetMetaData: AssetMetadata = {
+    assetId: '6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d',
+    name: 'Bitcoin',
+    ticker: 'BTC',
+    precision: 8
+}
+export const assetBalances = {
+    assetId: '6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d',
+    amount: 10000000000000000,
 }
