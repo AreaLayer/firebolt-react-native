584d183d46982df5500b68b0b39e796a9211f83c
22388o <22388o@users.noreply.github.com>
2025-02-25 09:34:32 -0300

    Fix treor tx

---
+++ src/app/hardware/trezor/tx.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+export interface GetAddressParams {
+  path: string;
+  showonTrezor: boolean;
+  chunkify: boolean;
+  useEventListener: boolean;
+  address: string;
+}
+
+export const defaultGetAddressParams: GetAddressParams = {
+  path: "m/44'/1'/0'/0/0",
+  showonTrezor: true,
+  chunkify: false,
+  useEventListener: false,
+  address: "",
+};
+export interface Hww  {
+  getAddress(params: GetAddressParams): Promise<any>;
+}
