e62ac573054202eb1465e28dc9225dccd4a02e55
22388o <22388o@users.noreply.github.com>
2025-05-09 06:04:24 -0300

    Fix

--- src/app/ip/vpn/client.ts
+++ src/app/ip/vpn/client.ts
@@ -1,4 +1,11 @@
 import client from "client";
+import initVPN from "InitVPN";
+import checkStatus from "CheckStatus";
+import shutdown from "Shutdown";
+
+
+function initVPN(): Promise<boolean> {
+    
 
 const client = {
     initVPN
