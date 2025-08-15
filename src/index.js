49a9853fc09790ae43a8f9b38dd099832cfccbba
22388o <22388o@users.noreply.github.com>
2025-08-15 07:53:40 -0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 /**
  * @format
  */
-import './shim';
+import '../shim';
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import App from '../App';
+import {name as appName} from '../app.json';
 import {coinjoin as Coinjoin } from './src/coinjoin';
 import {firebolt as Firebolt } from './src/firebolt';
-import { SilentPaymentGroup } from './src/app/SP/send';
+import { SilentPaymentGroup } from './app/SP/send';
 import {liquidwallet as LiquidWallet } from './src/app/LW/send';
 import {wallet as Wallet } from './src/app/W/send';
-import {SilentPaymentGroup as SilentPaymentGroup } from './src/app/SP/send';
+import {SilentPaymentGroup as SilentPaymentGroup } from './app/SP/send';
 import {HDWallet as HDWallet} from '.src/app/HDWallet';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
